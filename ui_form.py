63cb63eb6ee4748309e82d3f68e73db76ac17e19
Hagai Raja <hagairaja@telepix.net>
2025-02-05 22:33:13 +0000

    Implement base layout

--- ui_form.py
+++ ui_form.py
@@ -15,34 +15,141 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QPushButton,
-    QSizePolicy, QSpacerItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_Main(object):
     def setupUi(self, Main):
         if not Main.objectName():
             Main.setObjectName(u"Main")
-        Main.resize(800, 600)
+        Main.resize(805, 415)
+        self.horizontalLayout = QHBoxLayout(Main)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.frame = QFrame(Main)
         self.frame.setObjectName(u"frame")
-        self.frame.setGeometry(QRect(0, 0, 801, 45))
+        self.frame.setMaximumSize(QSize(300, 16777215))
+        self.frame.setStyleSheet(u"background-color: rgb(255, 255, 255);border: 0;")
         self.frame.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame.setFrameShadow(QFrame.Shadow.Raised)
-        self.horizontalLayout = QHBoxLayout(self.frame)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.pushButton_2 = QPushButton(self.frame)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.frame.setLineWidth(0)
+        self.verticalLayout = QVBoxLayout(self.frame)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame_4 = QFrame(self.frame)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setStyleSheet(u"border: 0;")
+        self.frame_4.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Shadow.Raised)
+        self.frame_4.setLineWidth(0)
+        self.verticalLayout_2 = QVBoxLayout(self.frame_4)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.frame_6 = QFrame(self.frame_4)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setMaximumSize(QSize(16777215, 30))
+        self.frame_6.setStyleSheet(u"border: 0;")
+        self.frame_6.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Shadow.Raised)
+        self.frame_6.setLineWidth(0)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_6)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(5, 0, 0, 0)
+        self.label = QLabel(self.frame_6)
+        self.label.setObjectName(u"label")
+        self.label.setStyleSheet(u"color: #4B4B4B;\n"
+"font-size: 20pt;")
+        self.label.setLineWidth(0)
 
-        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.horizontalLayout_2.addWidget(self.label)
 
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
-        self.horizontalLayout.addItem(self.horizontalSpacer)
+        self.verticalLayout_2.addWidget(self.frame_6)
 
-        self.pushButton = QPushButton(self.frame)
-        self.pushButton.setObjectName(u"pushButton")
+        self.frame_7 = QFrame(self.frame_4)
+        self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setStyleSheet(u"border: 0;")
+        self.frame_7.setFrameShape(QFrame.Shape.NoFrame)
+        self.frame_7.setFrameShadow(QFrame.Shadow.Raised)
+        self.frame_7.setLineWidth(0)
 
-        self.horizontalLayout.addWidget(self.pushButton)
+        self.verticalLayout_2.addWidget(self.frame_7)
+
+
+        self.verticalLayout.addWidget(self.frame_4)
+
+        self.frame_5 = QFrame(self.frame)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setStyleSheet(u"border: 0;")
+        self.frame_5.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Shadow.Raised)
+        self.frame_5.setLineWidth(0)
+        self.verticalLayout_3 = QVBoxLayout(self.frame_5)
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.frame_9 = QFrame(self.frame_5)
+        self.frame_9.setObjectName(u"frame_9")
+        self.frame_9.setMaximumSize(QSize(16777215, 30))
+        self.frame_9.setStyleSheet(u"border: 0;")
+        self.frame_9.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_9.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_9)
+        self.horizontalLayout_3.setSpacing(0)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(5, 0, 0, 0)
+        self.label_2 = QLabel(self.frame_9)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setStyleSheet(u"color: #4B4B4B;\n"
+"font-size: 20pt;")
+
+        self.horizontalLayout_3.addWidget(self.label_2)
+
+
+        self.verticalLayout_3.addWidget(self.frame_9)
+
+        self.frame_8 = QFrame(self.frame_5)
+        self.frame_8.setObjectName(u"frame_8")
+        self.frame_8.setStyleSheet(u"background-color: #D9D9D9;\n"
+"border-radius: 8px;")
+        self.frame_8.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_8.setFrameShadow(QFrame.Shadow.Raised)
+        self.frame_8.setLineWidth(0)
+
+        self.verticalLayout_3.addWidget(self.frame_8)
+
+
+        self.verticalLayout.addWidget(self.frame_5)
+
+
+        self.horizontalLayout.addWidget(self.frame)
+
+        self.line = QFrame(Main)
+        self.line.setObjectName(u"line")
+        self.line.setLineWidth(2)
+        self.line.setFrameShape(QFrame.Shape.VLine)
+        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.horizontalLayout.addWidget(self.line)
+
+        self.frame_2 = QFrame(Main)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setStyleSheet(u"background-color: rgb(255, 255, 255);border: 0;")
+        self.frame_2.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
+
+        self.horizontalLayout.addWidget(self.frame_2)
+
+        self.frame_3 = QFrame(Main)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setMaximumSize(QSize(400, 16777215))
+        self.frame_3.setStyleSheet(u"background-color: #333333;border: 0;")
+        self.frame_3.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
+
+        self.horizontalLayout.addWidget(self.frame_3)
 
 
         self.retranslateUi(Main)
@@ -51,8 +158,8 @@ class Ui_Main(object):
     # setupUi
 
     def retranslateUi(self, Main):
-        Main.setWindowTitle(QCoreApplication.translate("Main", u"Main", None))
-        self.pushButton_2.setText(QCoreApplication.translate("Main", u"PushButton", None))
-        self.pushButton.setText(QCoreApplication.translate("Main", u"PushButton", None))
+        Main.setWindowTitle(QCoreApplication.translate("Main", u"Audio AI", None))
+        self.label.setText(QCoreApplication.translate("Main", u"MENU", None))
+        self.label_2.setText(QCoreApplication.translate("Main", u"RECORDINGS", None))
     # retranslateUi
 
